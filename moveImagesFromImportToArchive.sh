8cca33c1fdfd024f441184ae2763733baa67d0e9
olli <oliver@porrmann.de>
2020-10-25 15:15:33 +0100

    insert endless loop to let container run

--- moveImagesFromImportToArchive.sh
+++ moveImagesFromImportToArchive.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-echo "delete mov files"
-find /import -name "*.mov" -type f -exec rm -rf {} \;
-echo "rename files"
-exiftool '-FileName<${DateTimeOriginal}%-c.%le' -d '%Y%m%d-%H%M%S' -r '/import'
-echo "set time and date of files"
-exiftool '-FileModifyDate<${DateTimeOriginal}' -r '/Multimedia/FotoArchiv/import'
-echo "move files"
-exiftool '-Directory<${DateTimeOriginal}' -d '/export/%Y/%m/%d' -r '/import'
+
+while true; do
+	echo "delete mov files"
+	find /import -name "*.mov" -type f -exec rm -rf {} \;
+	echo "rename files"
+	exiftool '-FileName<${DateTimeOriginal}%-c.%le' -d '%Y%m%d-%H%M%S' -r '/import'
+	echo "set time and date of files"
+	exiftool '-FileModifyDate<${DateTimeOriginal}' -r '/import'
+	echo "move files"
+	exiftool '-Directory<${DateTimeOriginal}' -d '/export/%Y/%m/%d' -r '/import'
+	echo "next run in 5 minutes"
+	sleep 300
+done
