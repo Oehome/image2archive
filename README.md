e57d093376baf6f711093b17b35e11ebe696d8f9
olli <oliver@porrmann.de>
2021-03-14 18:50:17 +0100

    changed to mqtt to trigger execution of import script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # image2archive
-simple script do rename and copy images based on their exif informations
+
+simple service to move images to a destination directory (YYYY/MM/DD) based on their exif data.
+
+A Python script is tigered via mqtt and starts a simple shell script that executes the command via the exiftool
+
+The whole thing is running here in a container on a QNAP
---
+++ pic2archive.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+echo "delete mov files"
+find /import -name "*.mov" -type f -exec rm -rf {} \;
+echo "rename files"
+exiftool '-FileName<${DateTimeOriginal}%-c.%le' -d '%Y%m%d-%H%M%S' -r /import
+echo "set time and date of files"
+exiftool '-FileModifyDate<${DateTimeOriginal}' -r /import
+echo "move files"
+exiftool '-Directory<${DateTimeOriginal}' -d "/export/%Y/%m/%d" -r /import
